bc955e9a000b7d97e0f03b7b8b1ce01b9bf01386
Saad Shahid <saad.techgeek@gmail.com>
2021-01-05 09:08:19 +0500

    commit changes

---
+++ src/context/AppReducer.js
@@ -0,0 +1,8 @@
+// its how we specify application state changes base on action to our context(store)
+
+export default (state,action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,28 @@
+//create context. 
+// if big application, profile context, shop etc, there will be multiple global state
+// 
+import React, { createContext, useReducer} from 'react';
+import AppReducer from './AppReducer'
+// Initial state
+const initialState= {
+    transactions:  [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+      ]
+}
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// other component can have access to state
+// Provider component
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer,initialState);
+    return (<GlobalContext.Provider value={{
+        transactions:state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>);
+}
